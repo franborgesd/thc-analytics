8d6d1743e557e3011c2e97316521dbde215dc479
Fran <francisco.borges.d@gmail.com>
2026-08-31 15:59:09 +0200

    Add comment to SQL query for clarity
    Added comment explaining the query's purpose.

--- models/marts/marts_orders_per_month_2026.sql
+++ models/marts/marts_orders_per_month_2026.sql
@@ -1,3 +1,4 @@
+-- Using the intermediate stage from orders, it counts distinct orders per month and displays them grouped and ordered monthly:
 SELECT 
     EXTRACT(MONTH FROM date) AS month,
     COUNT(DISTINCT orders_id) AS total_orders,
